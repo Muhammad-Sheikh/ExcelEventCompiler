51dd913c49ecc8df772ae99b066ba48cef6edbd9
Muhammad Sheikh <muhammad-sheikh@outlook.com>
2022-12-20 18:01:20 -0500

    Removed Top Comments

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name):2
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
