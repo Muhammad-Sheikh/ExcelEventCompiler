2cda8401cff2669e1ae85661b6d4fb73d0704e61
Muhammad Sheikh <muhammad-sheikh@outlook.com>
2022-12-20 19:55:33 -0500

    added basis of settings, directory storage and pickle file

--- main.py
+++ main.py
@@ -2,6 +2,27 @@
 import os
 import pickle
 
-open("settings.pk", "x")
-workingDir = input("Welcome to the AutoFormatter! Please add your working directory below!\n")
-with open(settings.pk)
+currentDir = os.getcwd()
+workingDir = currentDir + '\\testingGrounds'
+while not os.path.exists(workingDir):
+    os.mkdir(workingDir)
+
+pickleSettings = os.path.join(workingDir, 'Settings.pk')
+
+while not os.path.exists(os.path.join(workingDir, "Settings.pk")):
+    open(pickleSettings, 'x')
+
+with open(pickleSettings, 'wb') as pk:
+    pickle.dump(workingDir, pk)
+print(workingDir)
+
+workingDir = "lmao"
+print(workingDir)
+
+with open(pickleSettings, 'rb') as pk:
+    workingDir = pickle.load(pk)
+print(workingDir)
+
+
+#open("settings.pk", "x")
+#workingDir = input("Welcome to the AutoFormatter! Please add your working directory below!\n")
