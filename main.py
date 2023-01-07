b6fa0edabf63f9d79d2031260960d0d16ebc23a8
Muhammad Sheikh <muhammad-sheikh@outlook.com>
2023-01-07 03:09:33 -0500

    Finished the settings and intial config up, currently adding user input

--- main.py
+++ main.py
@@ -2,27 +2,41 @@
 import os
 import pickle
 
-currentDir = os.getcwd()
-workingDir = currentDir + '\\testingGrounds'
-while not os.path.exists(workingDir):
-    os.mkdir(workingDir)
 
-pickleSettings = os.path.join(workingDir, 'Settings.pk')
+def initialization():
+    # gets Current Directory, and makes a new folder in for all processed files
+    currentDir = os.getcwd()
+    workingDir = currentDir + '\\excelFormatter'
+    # if the folder does not exists, creates it.
+    while not os.path.exists(workingDir):
+        os.mkdir(workingDir)
 
-while not os.path.exists(os.path.join(workingDir, "Settings.pk")):
-    open(pickleSettings, 'x')
+    # Makes all needed files in working directory
+    Settings = os.path.join(workingDir, 'Settings.config')
+    programNames = os.path.join(workingDir, 'programNames.txt')
+    programDates = os.path.join(workingDir, 'dates.txt')
+    programDescription = os.path.join(workingDir, 'desc.txt')
+    programLocation = os.path.join(workingDir, 'location.txt')
+    programTiming = os.path.join(workingDir, 'timing.txt')
+    descriptorArray = [programNames, programDates, programDescription, programLocation, programTiming, pickleSettings]
 
-with open(pickleSettings, 'wb') as pk:
-    pickle.dump(workingDir, pk)
-print(workingDir)
+    # Checks and creates needed files
+    for descriptorFiles in descriptorArray:
+        while not os.path.exists(descriptorFiles):
+            open(descriptorFiles, 'x')
 
-workingDir = "lmao"
-print(workingDir)
+    # Writes Descriptor to the .config file, to allow it to use it for future usage.
+    with open(Settings, 'wb') as config:
+        pickle.dump(descriptorArray, config)
 
-with open(pickleSettings, 'rb') as pk:
-    workingDir = pickle.load(pk)
-print(workingDir)
+    # Reads off the settings file
+    with open(Settings, 'rb') as config:
+        descriptorArray = pickle.load(config)
+        descriptorArray = descriptorArray
 
+userInput = None
 
-#open("settings.pk", "x")
-#workingDir = input("Welcome to the AutoFormatter! Please add your working directory below!\n")
+userInput = input("Welcome to ExcelAutoFormatter! If this is your first time or you are coming back, enter GO to get started. If you would like to exit, please enter EXIT.")
+
+if userInput = 'GO' or 'go' or 'Go' or 'gO':
+
