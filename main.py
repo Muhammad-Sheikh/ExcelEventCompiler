ac874d6704a7023d3cce44a985e23b0e93c698d1
Muhammad Sheikh <muhammad-sheikh@outlook.com>
2023-01-07 19:54:50 -0500

    Finished up Beta 1.0.0, with all basic features implemented. Currently using text based inputs, may do another revision with a GUI. Also excluded some auto-made files.

--- main.py
+++ main.py
@@ -1,42 +1,70 @@
 
 import os
 import pickle
+import pandas as pd
+import time
 
+userInput = input("Welcome to ExcelAutoFormatter! If this is your first time or you are coming back, enter GO to get started. If you would like to exit, please enter EXIT.").lower()
 
-def initialization():
+if userInput == 'go':
     # gets Current Directory, and makes a new folder in for all processed files
     currentDir = os.getcwd()
     workingDir = currentDir + '\\excelFormatter'
-    # if the folder does not exists, creates it.
+    # if the folder does not exist, creates it.
     while not os.path.exists(workingDir):
         os.mkdir(workingDir)
 
-    # Makes all needed files in working directory
+    # Outlines the file format
     Settings = os.path.join(workingDir, 'Settings.config')
     programNames = os.path.join(workingDir, 'programNames.txt')
     programDates = os.path.join(workingDir, 'dates.txt')
     programDescription = os.path.join(workingDir, 'desc.txt')
     programLocation = os.path.join(workingDir, 'location.txt')
     programTiming = os.path.join(workingDir, 'timing.txt')
-    descriptorArray = [programNames, programDates, programDescription, programLocation, programTiming, pickleSettings]
+    descriptorsLocation = [programNames, programDates, programDescription, programLocation, programTiming, Settings]
 
     # Checks and creates needed files
-    for descriptorFiles in descriptorArray:
+    for descriptorFiles in descriptorsLocation:
         while not os.path.exists(descriptorFiles):
             open(descriptorFiles, 'x')
 
     # Writes Descriptor to the .config file, to allow it to use it for future usage.
     with open(Settings, 'wb') as config:
-        pickle.dump(descriptorArray, config)
+        pickle.dump(descriptorsLocation, config)
 
-    # Reads off the settings file
-    with open(Settings, 'rb') as config:
-        descriptorArray = pickle.load(config)
-        descriptorArray = descriptorArray
+elif userInput == 'exit':
+    exit()
+else:
+    print("Invalid Input. Exiting...")
+    time.sleep(5)
+    exit()
 
-userInput = None
+ # Reads off the settings file before any thing is used
+with open((os.path.join((os.getcwd() + '\\excelFormatter'), 'Settings.config')), 'rb') as config:
+    descriptorsLocation = pickle.load(config)
 
-userInput = input("Welcome to ExcelAutoFormatter! If this is your first time or you are coming back, enter GO to get started. If you would like to exit, please enter EXIT.")
+#Double checks that the files are filled in before reading and exporting, as to prevent an out of range error
+userInput = input("Are you ready to make the final file? If so, please enter Y (Make sure the files have the same "
+                  "amount of items & are filled in.)\n If not, enter N to exit.").lower()
+None if userInput == 'y' else exit()
 
-if userInput = 'GO' or 'go' or 'Go' or 'gO':
+#Opens all files for reading, as well as removes all newline separators
+currentNames = open(descriptorsLocation[0], "r").read().splitlines()
+currentDate = open(descriptorsLocation[1], "r").read().splitlines()
+currentDesc = open(descriptorsLocation[2], "r").read().splitlines()
+currentLocation = open(descriptorsLocation[3], "r").read().splitlines()
+currentTiming = open(descriptorsLocation[4], "r").read().splitlines()
 
+#Makes main data frame for import/exporting. Uses the list created from reading the files made.
+formattedEXCL= pd.DataFrame({"Program Name": currentNames, "Program Date": currentDate, "Program Description:": currentDesc, "Program Location:": currentLocation, "Program Timing:": currentTiming})
+
+#Asks user for what format the final file should be, and exports depending on the input.
+userInput = input("What format would you like? HTML or XLSX?").lower()
+if userInput == 'xlsx':
+    formattedEXCL.to_excel('./output.xlsx')
+elif userInput == 'html':
+    formattedEXCL.html('./output.html')
+else:
+    "Invalid Input. Exiting..."
+    time.sleep(5)
+    exit()
